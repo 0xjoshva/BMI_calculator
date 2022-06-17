af0024fa9964d51f5bf6fdd991eb847de16c3e3a
Joshua Steed <joshuasteed123@gmail.com>
2022-06-17 11:36:32 +0200

    caps

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ function calculate() {
   document.getElementById("BMI-header").innerHTML = Math.round(BMI * 100) / 100;
 
   if (BMI < 18.5) {
-    document.getElementById("calc-header").innerHTML = "Underweight";
+    document.getElementById("calc-header").innerHTML = "UNDERWEIGHT";
     document.getElementById("BMI-header").style.backgroundImage =
       " linear-gradient(to top, #0FE2C2, #00CBE3, #00AFFF, #008AFF, #0553FE)";
   }else if (BMI >= 18.5 && BMI <= 25){
-    document.getElementById("calc-header").innerHTML = "Normal";
+    document.getElementById("calc-header").innerHTML = "NORMAL";
     document.getElementById("BMI-header").style.backgroundImage =
       " linear-gradient(143deg, rgba(29,185,84,1) 35%, rgba(16,112,0,1) 100%)";
   }else if (BMI >= 25 && BMI <= 30){
-    document.getElementById("calc-header").innerHTML = "Overweight";
+    document.getElementById("calc-header").innerHTML = "OVERWEIGHT";
     document.getElementById("BMI-header").style.backgroundImage =
       " linear-gradient(156deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 0%, rgba(252,176,69,1) 100%)";
   }else if (BMI > 30){
-    document.getElementById("calc-header").innerHTML = "Obese";
+    document.getElementById("calc-header").innerHTML = "OBESE";
     document.getElementById("BMI-header").style.backgroundImage =
       " linear-gradient(156deg, rgba(210,0,0,1) 43%, rgba(144,0,0,1) 100%)";
   } 
